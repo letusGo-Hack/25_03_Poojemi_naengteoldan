0aed9e494a3fadbb1fa17875eca343e6f7982b55
COMI <admin@wincomi.com>
2025-07-19 10:55:00 +0900

    Initial commit

---
+++ Naengteoldan/NaengteoldanApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaengteoldanApp.swift
+//  Naengteoldan
+//
+//  Created by COMI on 7/19/25.
+//
+
+import SwiftUI
+
+@main
+struct NaengteoldanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
