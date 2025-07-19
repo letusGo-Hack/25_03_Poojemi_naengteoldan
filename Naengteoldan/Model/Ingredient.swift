f47679ce43dee01880d2a7921b0dba47aa65cb7d
COMI <admin@wincomi.com>
2025-07-19 13:29:26 +0900

    IngredientList UI 변경

---
+++ Naengteoldan/Model/Ingredient.swift
@@ -0,0 +1,13 @@
+//
+//  Ingredient.swift
+//  Naengteoldan
+//
+//  Created by COMI on 7/19/25.
+//
+
+import Foundation
+
+struct Ingredient: Identifiable, Hashable {
+  var id = UUID()
+  var name: String
+}
--- Naengteoldan/View/IngredientList.swift
+++ Naengteoldan/View/IngredientList.swift
@@ -8,42 +8,59 @@
 import SwiftUI
 
 struct IngredientList: View {
-    var body: some View {
-        List {
-            Text("계란")
-            Text("식빵")
-        }
-        .safeAreaBar(edge: .bottom) {
-            Button(role: .confirm) {
+  @State private var ingredients: [Ingredient] = [
+    Ingredient(name: "계란"),
+    Ingredient(name: "우유"),
+    Ingredient(name: "밀가루")
+  ]
 
-            } label: {
-                Label("AI 레시피 추천", systemImage: "apple.intelligence")
-                    .foregroundStyle(
-                        MeshGradient(width: 2, height: 2, points: [
-                            [0, 0], [1, 0],
-                            [0, 1], [1, 1]
-                        ], colors: [
-                            .pink, .indigo,
-                            .indigo, .blue
-                        ])
-                    )
-                    .padding(8)
-                    .fontWeight(.semibold)
-            }
-            .buttonStyle(.glass)
-            .padding(.bottom)
-        }
-        .navigationTitle("냉장고 재료")
-        .toolbar {
-            ToolbarItem(placement: .primaryAction) {
-                Button("추가", systemImage: "plus") {
+  @State private var selection = Set<UUID>()
 
-                }
-            }
-        }
+  var body: some View {
+    List(selection: $selection) {
+      ForEach(ingredients) { ingredient in
+        Text(ingredient.name)
+      }
     }
+    .environment(\.editMode, .constant(.active))
+    .safeAreaBar(edge: .bottom) {
+      recipeGenerateButton
+        .disabled(selection.isEmpty)
+        .padding(.bottom)
+    }
+    .navigationTitle("냉장고 재료")
+    .toolbar {
+      ToolbarItem(placement: .primaryAction) {
+        Button("추가", systemImage: "plus") {
+
+        }
+      }
+    }
+  }
+
+  private var recipeGenerateButton: some View {
+    Button(role: .confirm) {
+
+    } label: {
+      Label("AI 레시피 추천", systemImage: "apple.intelligence")
+        .foregroundStyle(
+          MeshGradient(width: 2, height: 2, points: [
+            [0, 0], [1, 0],
+            [0, 1], [1, 1]
+          ], colors: [
+            .pink, .indigo,
+            .indigo, .blue
+          ])
+        )
+        .padding(8)
+        .fontWeight(.semibold)
+    }
+    .buttonStyle(.glass)
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
+
+
